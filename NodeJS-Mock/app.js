c252c77cfe593e552a43f5f9a4bcd7668fb87c5e
dimych1 <dimych1@mail.ru>
2019-02-27 16:03:02 +0200

    Add find result

--- NodeJS-Mock/app.js
+++ NodeJS-Mock/app.js
@@ -1,20 +1,44 @@
 const express = require('express')
 const app = express()
+var bodyParser = require("body-parser");
 const port = 3000
 
 var fs = require("fs");
 console.log("\n *STARTING* \n");
 // Get content from file
 var filterData = fs.readFileSync("filterdata.json");
+var findResults = fs.readFileSync("findResults.json");
 // Define to JSON type
 var jsonFilterData = JSON.parse(filterData);
+var jsonFindResults = JSON.parse(findResults);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/users', (req, res) => res.send('BOOM!'))
 
-app.get('/filters', (req, res) =>{ 
+app.get('/filters', (req, res) => {
     res.send(JSON.stringify(jsonFilterData))
 })
+
+app.post('/filters', function (req, res) {
+    var includedAuthors = req.body.includedAuthors;
+    let resultFilter = new Array();
+    if (includedAuthors && includedAuthors.length > 0) {
+        includedAuthors.forEach(authorId => {
+            jsonFindResults.forEach(article => {
+                if (article.authorId == authorId) {
+                    resultFilter.push(article);
+                }
+            })
+        })
+    } else {
+        resultFilter = jsonFindResults
+    }
+    res.send(JSON.stringify(resultFilter));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
